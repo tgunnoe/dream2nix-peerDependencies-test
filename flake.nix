5bf013d4aafabc9c41577ae9ea29c1d8b27528f0
tgunnoe <t@gvno.net>
2022-04-03 13:16:51 -0400

    Initial commit 2

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
   inputs.dream2nix.url = "github:nix-community/dream2nix";
-  inputs.src.url = "github:tgunnoe/dream2nix-peerDependencies-test";
-  inputs.src.flake = false;
-  outputs = { self, dream2nix, src }@inputs:
+  outputs = { self, dream2nix }@inputs:
     let
       dream2nix = inputs.dream2nix.lib.init {
         systems = [ "x86_64-linux" ];
         config.projectRoot = ./. ;
       };
     in dream2nix.makeFlakeOutputs {
-      source = src;
+      source = ./.;
     };
 }
